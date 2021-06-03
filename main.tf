b02444c084cb605bde6c96be5b94b040b28d72fe
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-06-03 14:54:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
+  ami           = "${data.aws_ami.latest-ubuntu}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
 
   tags = {
     Name = "${var.name}"
+    ttl = "24h"
+    #owner = "jeremy" 
+    se-region = "C1" 
+    purpose = "Demo"
+    terraform = "True"
   }
 }
+
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
