c9fa91b60e3ebd19fe87f520151cf54fedc9fadf
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2022-06-22 12:35:43 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "public_dns" {
-  value = "${aws_instance.ubuntu.public_dns}"
+  value = "${aws_instance.app_server.public_dns}"
+}
+
+output "ubuntu_iteration" {
+  value = data.hcp_packer_iteration.ubuntu
+}
+
+output "ubuntu_us_east_2" {
+  value = data.hcp_packer_image.ubuntu_us_east_2
 }
