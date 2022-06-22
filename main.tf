085507d41a4089e20689e5e380665bb16a26c51b
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2022-06-22 12:47:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,33 +15,30 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = "${data.aws_ami.latest-ubuntu.id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+provider "hcp" {}
 
+data "hcp_packer_iteration" "ubuntu" {
+  bucket_name = "learn-packer-run-tasks"
+  channel     = "production"
+}
+
+data "hcp_packer_image" "ubuntu_us_east_2" {
+  bucket_name    = "learn-packer-run-tasks"
+  cloud_provider = "aws"
+  iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
+  region         = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.hcp_packer_image.ubuntu_us_east_2.cloud_image_id
+  instance_type = "t2.micro"
   tags = {
-    Name = "${var.name}"
+    Name = "Learn-HCP-Packer"
     ttl = "24h"
     owner = "jeremy" 
     se-region = "C1" 
     purpose = "Demo"
     terraform = "True"
-  }
-}
-
-data "aws_ami" "latest-ubuntu" {
-most_recent = true
-owners = ["099720109477"] # Canonical
-
-  filter {
-      name   = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-
-  filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
   }
 }
 
