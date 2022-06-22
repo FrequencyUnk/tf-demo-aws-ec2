2dfd479fd6cde38068147747f1ba161aedfd8054
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2022-06-22 12:34:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "us-east-2"
 }
+
+#variable "region" {
+#  type    = string
+#  default = "us-east-2"
+#}
 
 #variable "ami_id" {
 #  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
